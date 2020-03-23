232cece236f616299b9135925c44776c89cf9cb2
Nouw <offabio@outlook.com>
2020-03-23 14:58:52 +0100

    Removed unnecessary prints

--- engines/content_based_filtering.py
+++ engines/content_based_filtering.py
@@ -12,8 +12,6 @@ def content_based_filtering():
     file = open('engines/csv/content_based_filtering_data.csv', "w+")
     with file:
         for item in tqdm(result):
-            print(item)
-            # TODO: Vragen aan Rico hoe je 's in een where clause kan doen
             mycursor.execute("SELECT * FROM products WHERE brand = %s AND type = %s AND category = %s", (str(item[2]), str(item[3]), str(item[4])))
             similarItems = mycursor.fetchall()
             similarItems.pop(0)
